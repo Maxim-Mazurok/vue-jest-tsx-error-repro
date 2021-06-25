416a0f0c6cc6fed1f72756607806b91df9818e46
Maxim Mazurok <maxim@mazurok.com>
2021-06-26 00:00:41 +1000

    Apply fix https://github.com/vuetifyjs/vuetify/issues/13841#issuecomment-868475397

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
   moduleNameMapper: {
-    "^vuetify/lib(.*)": "vuetify/es5$1",
+    "^vuetify/lib": "vuetify/es5/entry-lib",
+    "^vuetify/lib/(.*)": "vuetify/es5/$1",
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
 };
