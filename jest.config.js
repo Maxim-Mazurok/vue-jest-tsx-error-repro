3b248a9a3b304e6fb421633ed9b204c8c84f5720
Maxim Mazurok <maxim@mazurok.com>
2021-06-25 22:17:45 +1000

    sync moduleNameMapper with the comment

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
-  moduleNameMapper: { "^vuetify/lib(/?.*)": "vuetify/es5/$1" },
+  moduleNameMapper: { "^vuetify/lib(.*)": "vuetify/es5$1" },
 };
